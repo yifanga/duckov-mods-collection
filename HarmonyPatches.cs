ff6ba1ebbbbd31b22952a6b6299d6fe74ea518ba
ljmeng <menglingjia0214@gmail.com>
2025-11-03 02:14:09 +0800

    feat: 实现了高效地面物品搜索

---
+++ HarmonyPatches.cs
@@ -0,0 +1,164 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using Duckov.UI;
+using HarmonyLib;
+using ItemStatsSystem;
+using SodaCraft.Localizations;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace LootNearbyItem
+{
+
+
+
+    [HarmonyPatch]
+    public static class WeightBarComplex_Patch
+    {
+
+        // 存储上次的重量值用于比较
+        // 使用 ConditionalWeakTable 存储每个实例的状态
+        private static readonly ConditionalWeakTable<WeightBarHUD, InstanceState> stateTable =
+            new ConditionalWeakTable<WeightBarHUD, InstanceState>();
+
+        // 每个实例的状态类
+        private class InstanceState
+        {
+            public float LastWeight { get; set; } = -1f;
+            public float LastMaxWeight { get; set; } = -1f;
+        }
+
+        [HarmonyPatch(typeof(WeightBarHUD), "Update")]
+        [HarmonyPostfix]
+        private static void ModifyWeightText(WeightBarHUD __instance)
+        {
+
+            // 获取当前实例的状态（如果不存在则创建）
+            InstanceState state = stateTable.GetValue(__instance, inst => new InstanceState());
+
+            // 获取私有字段值
+            float weight = Traverse.Create(__instance).Field("weight").GetValue<float>();
+            float maxWeight = Traverse.Create(__instance).Field("maxWeight").GetValue<float>();
+
+            // 检查重量是否变化
+            bool weightChanged = !Mathf.Approximately(weight, state.LastWeight) ||
+                                !Mathf.Approximately(maxWeight, state.LastMaxWeight);
+
+            // 只在重量变化时更新
+            if (weightChanged)
+            {
+                // 更新存储的重量值
+                state.LastWeight = weight;
+                state.LastMaxWeight = maxWeight;
+
+                // 只在超重时添加说明
+                string originalText = string.Format(__instance.weightTextFormat, weight, maxWeight);
+                string extraText = getWeightTextByLang(weight, maxWeight);
+                __instance.weightText.text = $"{originalText} {extraText}";
+                RectTransform rectTransform = (RectTransform)__instance.transform;
+                if (rectTransform.sizeDelta.x == 200 && rectTransform.sizeDelta.y == 10)
+                {
+                    rectTransform.sizeDelta = new Vector2(280, 10);
+                }
+            }
+        }
+
+        private static string getWeightTextByLang(float weight, float maxWeight)
+        {
+            var currentLanguage = LocalizationManager.CurrentLanguage;
+            if (UnityEngine.SystemLanguage.ChineseSimplified.Equals(currentLanguage)
+            || UnityEngine.SystemLanguage.ChineseTraditional.Equals(currentLanguage))
+            {
+                return calculateTextByWeight(weight, maxWeight);
+            }
+            return calculateOtherTextByWeight(weight, maxWeight);
+        }
+
+
+        private static string calculateTextByWeight(float weight, float maxWeight)
+        {
+            // 获取当前重量
+            float currentWeight = weight;
+
+            // 获取最大重量（使用反射）
+            float lightWeight = maxWeight * 0.25f;
+            float middleWeight = maxWeight * 0.5f;
+            float superHeavyWeight = maxWeight * 0.75f;
+
+            // 更新文本内容
+            if (currentWeight > maxWeight)
+            {
+                return $"(超重{currentWeight - maxWeight:0.#}kg)";
+            }
+            else if (currentWeight > maxWeight * 0.9f) // 接近超重时
+            {
+
+                return $"(距超重{maxWeight - currentWeight:0.#}kg)";
+            }
+            else if (currentWeight > superHeavyWeight) // >=负重时
+            {
+                return $"(负重{currentWeight - superHeavyWeight:0.#}kg)";
+            }
+            else if (currentWeight > middleWeight) // <=负重时
+            {
+                return $"(距负重{superHeavyWeight - currentWeight:0.#}kg)";
+            }
+            else if (currentWeight > lightWeight) // >=轻盈时
+            {
+                return $"(距轻盈{currentWeight - lightWeight:0.#}kg)";
+            }
+            else if (currentWeight < lightWeight) // <轻盈时
+            {
+                return $"(轻盈{lightWeight - currentWeight:0.#}kg)";
+            }
+            else
+            {
+                return $"(剩余容量{currentWeight:0.#}kg)";
+            }
+        }
+
+        private static string calculateOtherTextByWeight(float weight, float maxWeight)
+        {
+            // 获取当前重量
+            float currentWeight = weight;
+
+            // 获取最大重量（使用反射）
+            float lightWeight = maxWeight * 0.25f;
+            float middleWeight = maxWeight * 0.5f;
+            float superHeavyWeight = maxWeight * 0.75f;
+
+            // 更新文本内容
+            if (currentWeight > maxWeight)
+            {
+                return $"[ | | ]←{currentWeight - maxWeight:0.#}kg";
+            }
+            else if (currentWeight > maxWeight * 0.9f) // 接近超重时
+            {
+
+                return $"[ | | {maxWeight - currentWeight:0.#}kg→]";
+            }
+            else if (currentWeight > superHeavyWeight) // >=负重时
+            {
+                return $"[ | |←{currentWeight - superHeavyWeight:0.#}kg ]";
+            }
+            else if (currentWeight > middleWeight) // <=负重时
+            {
+                return $"[ | {superHeavyWeight - currentWeight:0.#}kg→| ]";
+            }
+            else if (currentWeight > lightWeight) // >=轻盈时
+            {
+                return $"[ |←{currentWeight - lightWeight:0.#}kg | ]";
+            }
+            else if (currentWeight < lightWeight) // <轻盈时
+            {
+                return $"[ {lightWeight - currentWeight:0.#}kg→| | ]";
+            }
+            else
+            {
+                return $"[ {currentWeight:0.#}kg→]";
+            }
+        }
+
+    }
+}
---
+++ ModBehaviour.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using Duckov.UI;
+using Duckov.Utilities;
+using ItemStatsSystem;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using HarmonyLib;
+using System.Reflection;
+using Duckov.MasterKeys.UI;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LootNearbyItem
+{
+
+    public class ModBehaviour : Duckov.Modding.ModBehaviour
+    {
+
+        private const string HarmonyId = "duckovMods.TagInventoryWeight";
+
+        // private Harmony harmony;
+
+        void OnEnable()
+        {
+            InteractHUD h;
+            InteractSelectionHUD a;
+            InteractablePickup p;
+            // Debug.Log("TagInventoryWeight Loaded!!!");
+            // // 创建Harmony实例
+            // harmony = new Harmony(HarmonyId);
+            // // 直接应用补丁
+            // harmony.PatchAll(Assembly.GetExecutingAssembly());
+
+        }
+
+
+        void OnDisable()
+        {
+            // if (harmony != null)
+            // {
+            //     // 更精确的卸载方式 - 只移除本mod的补丁
+            //     harmony.UnpatchAll(HarmonyId);
+            //     harmony = null;
+            //     Debug.Log("Harmony patches removed");
+            // }
+
+        }
+
+        void Update()
+        {
+            // 检测空格键按下
+            if (Input.GetKeyDown(KeyCode.H))
+            {
+                Debug.Log("H key pressed!");
+                // 在这里执行你的逻辑
+                LogPickUps();
+            }
+        }
+
+        void LogPickUps()
+        {
+            CharacterMainControl? main = LevelManager.Instance?.MainCharacter;
+            if (main != null)
+            {
+                Debug.Log($"main pos at {main.transform.position}");
+            }
+            else
+            {
+                Debug.Log("Test no main");
+                return;
+            }
+
+            InteractableBase? around = main.interactAction.MasterInteractableAround;
+            if (null != around)
+            {
+                foreach (InteractableBase pickupBase in around.GetInteractableList())
+                {
+                    if (pickupBase is InteractablePickup pickup)
+                    {
+                        Item item = pickup.ItemAgent.Item;
+                        Debug.Log("find Interactable item " + item.name + " " + pickup.isActiveAndEnabled + " at pos " + pickup.ItemAgent.transform.position);
+                    }
+                }
+                var otherGroup = SearchPickUpAround();
+                foreach (InteractablePickup pickup in otherGroup)
+                {
+                    Item item = pickup.ItemAgent.Item;
+                    Debug.Log("find other item " + item.name + " " + pickup.isActiveAndEnabled + " at pos " + pickup.ItemAgent.transform.position + " distance: " + Vector3.Distance(main.transform.position, item.transform.position));
+                }
+
+            }
+
+            //太慢了，而且会卡
+            // InteractablePickup[] pickups = UnityEngine.Object.FindObjectsOfType<InteractablePickup>();
+            // foreach (InteractablePickup pickup in pickups)
+            // {
+            //     if (pickup != null)
+            //     {
+            //         Item item = pickup.ItemAgent.Item;
+            //         Debug.Log("find item" + item.name + " " + pickup.isActiveAndEnabled);
+            //         Debug.Log(pickup.ItemAgent);
+            //         CharacterMainControl main = CharacterMainControl.Main;
+            //     }
+            // }
+        }
+
+
+        public static List<InteractablePickup> SearchPickUpAround()
+        {
+            Collider[] colliders = new Collider[100];
+            LayerMask interactLayers = 1 << LayerMask.NameToLayer("Interactable");
+            CharacterMainControl? main = LevelManager.Instance?.MainCharacter;
+
+            if (null == main || !main.IsMainCharacter)
+            {
+                return new List<InteractablePickup>();
+            }
+
+            int num = Physics.OverlapSphereNonAlloc(main.transform.position + Vector3.up * 0.5f + main.CurrentAimDirection * 0.2f, 0.3f, colliders, interactLayers);
+            if (num <= 0)
+            {
+                return new List<InteractablePickup>();
+            }
+
+            HashSet<InteractablePickup> uniqueItems = new HashSet<InteractablePickup>();
+            for (int i = 0; i < num; i++)
+            {
+                Collider collider = colliders[i];
+                InteractablePickup tmp = collider.GetComponent<InteractablePickup>();
+                if (null != tmp)
+                {
+                    uniqueItems.Add(tmp);
+                }
+
+            }
+            return uniqueItems.ToList();
+        }
+
+        
+
+    }
+}
